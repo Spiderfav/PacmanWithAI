f0e2834cd6809a298b9a7b156bff078fb691d42c
Favinha Rui (2021) ZKAC432 <rui.favinha.2021@live.rhul.ac.uk>
2023-10-11 14:07:14 +0100

    Created start of grid drawing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -14,7 +15,34 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	// Clear the screen
+	screen.Fill(color.White)
+	DrawMaze(screen)
+
+}
+
+func DrawMaze(screen *ebiten.Image) {
+
+	var i float64
+
+	for i = 20; i < 200; i += 20 {
+		DrawSquare(screen, i)
+		//fmt.Println(sum)
+	}
+}
+
+func DrawSquare(screen *ebiten.Image, x float64) {
+	// ebitenutil.DrawLine(screen, 25, 25, 25, 45, color.Black)
+	// ebitenutil.DrawLine(screen, 25, 25, 45, 25, color.Black)
+	// ebitenutil.DrawLine(screen, 45, 45, 25, 45, color.Black)
+	// ebitenutil.DrawLine(screen, 45, 45, 45, 25, color.Black)
+
+	y := x + 20
+
+	ebitenutil.DrawLine(screen, x, x, x, y, color.Black)
+	ebitenutil.DrawLine(screen, x, x, y, x, color.Black)
+	ebitenutil.DrawLine(screen, y, y, x, y, color.Black)
+	ebitenutil.DrawLine(screen, y, y, y, x, color.Black)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
