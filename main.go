48f6e7f03d3be347c8b873c3813edfb83e58135f
Favinha Rui (2021) ZKAC432 <rui.favinha.2021@live.rhul.ac.uk>
2023-10-11 15:16:01 +0100

    Created maze square object

--- main.go
+++ main.go
@@ -8,6 +8,20 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+type MazeSquare struct {
+	X_Value                                       float64
+	LeftSquare, DownSquare, RightSquare, UpSquare *MazeSquare
+}
+
+func (square MazeSquare) DrawSquare(screen *ebiten.Image) {
+	y := square.X_Value + 20
+
+	ebitenutil.DrawLine(screen, square.X_Value, square.X_Value, square.X_Value, y, color.Black)
+	ebitenutil.DrawLine(screen, square.X_Value, square.X_Value, y, square.X_Value, color.Black)
+	ebitenutil.DrawLine(screen, y, y, square.X_Value, y, color.Black)
+	ebitenutil.DrawLine(screen, y, y, y, square.X_Value, color.Black)
+}
+
 type Game struct{}
 
 func (g *Game) Update() error {
@@ -23,26 +37,24 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 func DrawMaze(screen *ebiten.Image) {
 
-	var i float64
+	var square1 = MazeSquare{20, nil, nil, nil, nil}
 
-	for i = 20; i < 200; i += 20 {
-		DrawSquare(screen, i)
-		//fmt.Println(sum)
-	}
+	square1.DrawSquare(screen)
+
+	//var i float64
+
+	// for i = 20; i < 200; i += 20 {
+	// 	DrawSquareOld(screen, i)
+	// 	//fmt.Println(sum)
+	// }
 }
 
-func DrawSquare(screen *ebiten.Image, x float64) {
-	// ebitenutil.DrawLine(screen, 25, 25, 25, 45, color.Black)
-	// ebitenutil.DrawLine(screen, 25, 25, 45, 25, color.Black)
-	// ebitenutil.DrawLine(screen, 45, 45, 25, 45, color.Black)
-	// ebitenutil.DrawLine(screen, 45, 45, 45, 25, color.Black)
+func DrawSquareOld(screen *ebiten.Image, x float64) {
+	ebitenutil.DrawLine(screen, 25, 25, 25, 45, color.Black)
+	ebitenutil.DrawLine(screen, 25, 25, 45, 25, color.Black)
+	ebitenutil.DrawLine(screen, 45, 45, 25, 45, color.Black)
+	ebitenutil.DrawLine(screen, 45, 45, 45, 25, color.Black)
 
-	y := x + 20
-
-	ebitenutil.DrawLine(screen, x, x, x, y, color.Black)
-	ebitenutil.DrawLine(screen, x, x, y, x, color.Black)
-	ebitenutil.DrawLine(screen, y, y, x, y, color.Black)
-	ebitenutil.DrawLine(screen, y, y, y, x, color.Black)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
