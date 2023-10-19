756dad9a1d1507cb79b56563bd730d77408a809f
Favinha Rui (2021) ZKAC432 <rui.favinha.2021@live.rhul.ac.uk>
2023-10-19 14:12:18 +0100

    Completed Random Maze Generation

--- MazeGeneration.go
+++ MazeGeneration.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"math/rand"
 
@@ -8,52 +9,219 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
-var strokeWidth float32 = 1
+var gameGrid = CreateMaze()
 
-func DFS(gridSize int, gameGrid [][]MazeSquare) {
+func DFS() [8][8]MazeSquare {
 
-	var visitedGrid [][]int
-	var stack []MazeSquare
+	gridSize := 7
+
+	// printGrid(gameGrid)
+
+	var stack []*MazeSquare
+
+	var nextNodeNoGrid *MazeSquare
 
 	// Randomly selecting a node
-	startPoint := rand.Intn(gridSize)
-	startNode := gameGrid[startPoint][startPoint]
+	startPointX := rand.Intn(gridSize)
+	startPointY := rand.Intn(gridSize)
+
+	startNode := &gameGrid[startPointX][startPointY]
 
 	// Appending node to stack
 	stack = append(stack, startNode)
 
-	// Marking node as visited
-	visitedGrid[startPoint][startPoint] = 1
+	for len(stack) != 0 {
+		currentAllNodes := 0
 
-	// Choose random direction to go in
-	moveOptions := [4]MazeSquare{*startNode.Left, *startNode.Down, *startNode.Right, *startNode.Up}
+		//fmt.Println("Start Node After Assignment ", startNode)
 
-	direction := moveOptions[rand.Intn(3)]
+		// Marking node as visited
+		gameGrid[int(startNode.YCoordinate/20)-1][int(startNode.XCoordinate/20)-1].Visited = true
 
-	// If direction picked does not have a node, pick another direction
-	for (direction == MazeSquare{}) {
-		direction = moveOptions[rand.Intn(3)]
+		//printGrid(gameGrid)
+
+		//fmt.Println("Start node = ", startNode)
+
+		// Choose random direction to go in
+		nextNodeNoGrid = chooseDirection(int(startNode.XCoordinate), int(startNode.YCoordinate))
+
+		nextNode := gameGrid[int(nextNodeNoGrid.YCoordinate/20)-1][int(nextNodeNoGrid.XCoordinate/20)-1]
+
+		//fmt.Println("Node chosen: ", nextNode)
+
+		//fmt.Println("Is visited: ", nextNode.Visited)
+
+		//time.Sleep(2 * time.Second)
+
+		if nextNode.Visited {
+
+			//fmt.Println("Oh no it's been visited!")
+
+			currentAllNodes = 1
+			startNode = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+		}
+
+		if currentAllNodes == 1 {
+			continue
+		}
+
+		//fmt.Println("Start Node Before ", startNode)
+
+		startNode = &MazeSquare{}
+
+		startNode = &nextNode
+
+		//fmt.Println("Start Node After ", startNode)
+
+		// Appending node to stack
+		stack = append(stack, startNode)
+
+		//moveOptions := [4]MazeSquare{*startNode.Left, *startNode.Down, *startNode.Right, *startNode.Up}
+
+		// If direction picked does not have a node, pick another direction
 	}
+
+	//fmt.Println("Finished function!!!")
+	return gameGrid
 
 }
 
-func breakLine(current MazeSquare, screen *ebiten.Image, directionToBreak int) {
+func DrawSquare(screen *ebiten.Image, squareToDraw MazeSquare) {
 
-	// 0 = Left, 1 = Down, 2 = Right, 3 = Up
+	fmt.Println("Drawing this square now: ", squareToDraw)
 
-	switch directionToBreak {
+	var strokeWidth float32 = 1
+
+	if squareToDraw.HasDown {
+		vector.StrokeLine(screen, squareToDraw.XCoordinate, squareToDraw.YCoordinate+20, squareToDraw.XCoordinate+20, squareToDraw.YCoordinate+20, strokeWidth, color.Black, false)
+	}
+
+	if squareToDraw.HasRight {
+		vector.StrokeLine(screen, squareToDraw.XCoordinate+20, squareToDraw.YCoordinate, squareToDraw.XCoordinate+20, squareToDraw.YCoordinate+20, strokeWidth, color.Black, false)
+	}
+
+	if squareToDraw.HasLeft {
+		vector.StrokeLine(screen, squareToDraw.XCoordinate, squareToDraw.YCoordinate, squareToDraw.XCoordinate, squareToDraw.YCoordinate+20, strokeWidth, color.Black, false)
+	}
+
+	if squareToDraw.HasUp {
+		vector.StrokeLine(screen, squareToDraw.XCoordinate, squareToDraw.YCoordinate, squareToDraw.XCoordinate+20, squareToDraw.YCoordinate, strokeWidth, color.Black, false)
+	}
+
+	fmt.Println("Finished the function!")
+	fmt.Println(" ")
+
+}
+
+func chooseDirection(x int, y int) *MazeSquare {
+	startNode := gameGrid[(y/20)-1][(x/20)-1]
+
+	//fmt.Println("Getting from Game grid = ", startNode)
+
+	var options []int
+
+	var direction *MazeSquare
+
+	directionNumber := 0
+
+	if ((y / 20) - 1) != 7 {
+		if (gameGrid[((y/20)-1)+1][(x/20)-1] != MazeSquare{}) && !gameGrid[((y/20)-1)+1][(x/20)-1].Visited {
+			//fmt.Println("Is Visited in Direction Down: ", startNode.Down.Visited)
+			options = append(options, 1)
+		}
+	}
+
+	if ((y / 20) - 1) != 0 {
+		if (gameGrid[((y/20)-1)-1][(x/20)-1] != MazeSquare{}) && !gameGrid[((y/20)-1)-1][(x/20)-1].Visited {
+			//fmt.Println("Is Visited in Direction Up: ", startNode.Up.Visited)
+
+			options = append(options, 3)
+		}
+	}
+
+	if ((x / 20) - 1) != 7 {
+		if (gameGrid[(y/20)-1][((x/20)-1)+1] != MazeSquare{}) && !gameGrid[(y/20)-1][((x/20)-1)+1].Visited {
+			//fmt.Println("Is Visited in Direction Right: ", startNode.Right.Visited)
+
+			options = append(options, 2)
+		}
+	}
+
+	if ((x / 20) - 1) != 0 {
+		if (gameGrid[(y/20)-1][((x/20)-1)-1] != MazeSquare{}) && !gameGrid[(y/20)-1][((x/20)-1)-1].Visited {
+			//fmt.Println("Is Visited in Direction Left: ", startNode.Left.Visited)
+
+			options = append(options, 0)
+		}
+	}
+
+	//fmt.Println("Array: ", options)
+
+	if len(options) == 0 {
+		return &gameGrid[(y/20)-1][(x/20)-1]
+	}
+
+	nodeChosenPos := rand.Intn(len(options))
+
+	directionNumber = options[nodeChosenPos]
+
+	switch directionNumber {
+
 	case 0:
-		vector.StrokeLine(screen, current.XCoordinate, current.YCoordinate, current.Down.XCoordinate, current.Down.YCoordinate, strokeWidth, color.White, false)
+		// if startNode.Left == nil {
+		// 	direction = MazeSquare{}
+		// 	break
+		// }
+		//fmt.Println("Left: ", startNode.Left)
+
+		direction = startNode.Left
+
+		//fmt.Println("Direcion: ", direction)
+
+		gameGrid[(y/20)-1][(x/20)-1].HasLeft = false
+		gameGrid[(y/20)-1][((x/20)-1)-1].HasRight = false
 
 	case 1:
-		vector.StrokeLine(screen, current.Down.XCoordinate, current.Down.YCoordinate, current.Down.Right.XCoordinate, current.Down.Right.YCoordinate, strokeWidth, color.White, false)
+		// if startNode.Down == nil {
+		// 	direction = MazeSquare{}
+		// 	break
+		// }
+
+		//fmt.Println("Down: ", startNode.Down)
+
+		direction = startNode.Down
+		//fmt.Println("Direcion: ", direction)
+		gameGrid[(y/20)-1][(x/20)-1].HasDown = false
+		gameGrid[((y/20)-1)+1][(x/20)-1].HasUp = false
 
 	case 2:
-		vector.StrokeLine(screen, current.Right.XCoordinate, current.Right.YCoordinate, current.Right.Down.XCoordinate, current.Right.Down.YCoordinate, strokeWidth, color.White, false)
+		// if startNode.Right == nil {
+		// 	direction = MazeSquare{}
+		// 	break
+		// }
+		//fmt.Println("Right: ", startNode.Right)
+		direction = startNode.Right
+		//fmt.Println("Direcion: ", direction)
+		gameGrid[(y/20)-1][(x/20)-1].HasRight = false
+		gameGrid[(y/20)-1][((x/20)-1)+1].HasLeft = false
 
 	case 3:
-		vector.StrokeLine(screen, current.XCoordinate, current.YCoordinate, current.Right.XCoordinate, current.Right.YCoordinate, strokeWidth, color.White, false)
+
+		// if startNode.Up == nil {
+		// 	direction = MazeSquare{}
+		// 	break
+		// }
+		//fmt.Println("Up: ", startNode.Up)
+		direction = startNode.Up
+		//fmt.Println("Direcion: ", direction)
+		gameGrid[(y/20)-1][(x/20)-1].HasUp = false
+		gameGrid[((y/20)-1)-1][(x/20)-1].HasDown = false
 
 	}
 
+	// }
+
+	return direction
 }
--- main.go
+++ main.go
@@ -5,33 +5,25 @@ import (
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 type MazeSquare struct {
 	XCoordinate float32
 	YCoordinate float32
 	Left        *MazeSquare
+	HasLeft     bool
 	Down        *MazeSquare
+	HasDown     bool
 	Right       *MazeSquare
+	HasRight    bool
 	Up          *MazeSquare
-}
-
-var gameGrid [8][8]MazeSquare
-
-func (square MazeSquare) DrawSquare(screen *ebiten.Image, x float32, y float32) {
-
-	var strokeWidth float32 = 1
-
-	vector.StrokeLine(screen, x, y, x+20, y, strokeWidth, color.Black, false)
-	vector.StrokeLine(screen, x+20, y, x+20, y+20, strokeWidth, color.Black, false)
-
-	vector.StrokeLine(screen, x+20, y+20, x, y+20, strokeWidth, color.Black, false)
-	vector.StrokeLine(screen, x, y+20, x, y, strokeWidth, color.Black, false)
-
+	HasUp       bool
+	Visited     bool
 }
 
 type Game struct{}
+
+var gameGridDFS [8][8]MazeSquare = DFS()
 
 func (g *Game) Update() error {
 	return nil
@@ -40,42 +32,56 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	// Clear the screen
 	screen.Fill(color.White)
+	// DFS(8, gameGrid)
+
 	DrawMaze(screen)
-	//breakLine(gameGrid[3][3], screen)
+
 }
 
 func DrawMaze(screen *ebiten.Image) {
 
-	var i, j float32
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			DrawSquare(screen, gameGridDFS[i][j])
+		}
+	}
+}
+
+func CreateMaze() [8][8]MazeSquare {
+	var gameGrid [8][8]MazeSquare
+
+	var x, y float32
 
 	var squareLengthX, squareLengthY float32 = 20, 20
 
-	for j = 0; j < 8; j += 1 {
+	for y = 0; y < 8; y++ {
 
-		for i = 0; i < 8; i += 1 {
+		for x = 0; x < 8; x++ {
 
 			// Using i + 1 and j + 1 as this is calculating the square size and as it starts by 0, we need to add one to the normal counter
-			var square = MazeSquare{squareLengthX * (i + 1), squareLengthY * (j + 1), nil, nil, nil, nil}
-			square.DrawSquare(screen, squareLengthX*(i+1), squareLengthY*(j+1))
+			var square = MazeSquare{squareLengthX * (x + 1), squareLengthY * (y + 1), nil, true, nil, true, nil, true, nil, true, false}
+			//square.DrawSquare(screen, squareLengthX*(i+1), squareLengthY*(j+1))
 
-			if i > 0 {
-				square.Left = &gameGrid[int(j)][int(i-1)]
-				gameGrid[int(j)][int(i-1)].Right = &square
+			gameGrid[int(y)][int(x)] = square
+
+			if x > 0 {
+				//fmt.Println("Square ", square)
+				gameGrid[int(y)][int(x)].Left = &gameGrid[int(y)][int(x-1)]
+				//fmt.Println("Square next ", square)
+				gameGrid[int(y)][int(x-1)].Right = &gameGrid[int(y)][int(x)]
 			}
 
-			if j > 0 || (i == 0 && j > 0) {
-				square.Up = &gameGrid[int(j-1)][int(i)]
-				gameGrid[int(j-1)][int(i)].Down = &square
+			if y > 0 {
+				gameGrid[int(y)][int(x)].Up = &gameGrid[int(y-1)][int(x)]
+				gameGrid[int(y-1)][int(x)].Down = &gameGrid[int(y)][int(x)]
 
 			}
-
-			gameGrid[int(i)][int(j)] = square
 
 		}
+
 	}
 
-	//fmt.Println(gameGrid[3][3])
-	// fmt.Println(gameGrid[0][0].Down.YCoordinate)
+	return gameGrid
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -98,6 +104,7 @@ The & Operator
 
 The * Operator
 
-	* goes in front of a variable that holds a memory address and resolves it (it is therefore the counterpart to the & operator). It goes and gets the thing that the pointer was pointing at
+	* goes in front of a variable that holds a memory address and resolves it (it is therefore the counterpart to the & operator).
+	It goes and gets the thing that the pointer was pointing at
 
 */
