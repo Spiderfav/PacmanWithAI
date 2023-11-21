3f5ec9524a54ddde7dcdd4802bde18cb7c977c79
Favinha Rui (2021) ZKAC432 <rui.favinha.2021@live.rhul.ac.uk>
2023-11-21 11:42:30 +0000

    Moved testing files into folder ;added more tests

---
+++ testing/AbsolutePath_test.go
@@ -0,0 +1,160 @@
+package testing
+
+import (
+	"testing"
+
+	"gitlab.cim.rhul.ac.uk/zkac432/PROJECT/algorithms"
+	"gitlab.cim.rhul.ac.uk/zkac432/PROJECT/mazegrid"
+)
+
+var mazeSizeOriginal = 4
+
+func TestOneNode(t *testing.T) {
+	var pathToTest = []mazegrid.MazeSquare{mazegrid.CreateBlankSquare()}
+
+	path, weight := algorithms.AbsolutePath(pathToTest)
+	if len(path) != 1 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", len(path), "1")
+	} else if weight != 0 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", weight, "0")
+	}
+}
+
+func TestStartAndEndDikstras(t *testing.T) {
+	var gameGridDFS [][]mazegrid.MazeSquare = algorithms.DFS(mazeSizeOriginal)
+
+	var dijkstrasPath = algorithms.Dijkstras(gameGridDFS, 20, 20, 20*mazeSizeOriginal, 20*mazeSizeOriginal)
+	var absolutePathDijkstras, _ = algorithms.AbsolutePath(dijkstrasPath)
+
+	if absolutePathDijkstras[0].XCoordinate != float32(20*mazeSizeOriginal) && absolutePathDijkstras[0].YCoordinate != float32(20*mazeSizeOriginal) {
+		t.Errorf("Result was incorrect, got X value: %f, Y value: %f ; Want X: %d, Y: %d.", absolutePathDijkstras[0].XCoordinate, absolutePathDijkstras[0].YCoordinate, 20*mazeSizeOriginal, 20*mazeSizeOriginal)
+
+	} else if absolutePathDijkstras[len(absolutePathDijkstras)-1].XCoordinate != float32(20) && absolutePathDijkstras[len(absolutePathDijkstras)-1].YCoordinate != float32(20) {
+		t.Errorf("Result was incorrect, got X value: %f, Y value: %f ; Want X: %d, Y: %d.", absolutePathDijkstras[0].XCoordinate, absolutePathDijkstras[0].YCoordinate, 20*mazeSizeOriginal, 20*mazeSizeOriginal)
+
+	}
+}
+
+func TestStartAndEndAStar(t *testing.T) {
+	var gameGridDFS [][]mazegrid.MazeSquare = algorithms.DFS(mazeSizeOriginal)
+
+	var dijkstrasPath = algorithms.AStar(gameGridDFS, 20, 20, 20*mazeSizeOriginal, 20*mazeSizeOriginal)
+	var absolutePathAStar, _ = algorithms.AbsolutePath(dijkstrasPath)
+
+	if absolutePathAStar[0].XCoordinate != float32(20*mazeSizeOriginal) && absolutePathAStar[0].YCoordinate != float32(20*mazeSizeOriginal) {
+		t.Errorf("Result was incorrect, got X value: %f, Y value: %f ; Want X: %d, Y: %d.", absolutePathAStar[0].XCoordinate, absolutePathAStar[0].YCoordinate, 20*mazeSizeOriginal, 20*mazeSizeOriginal)
+
+	} else if absolutePathAStar[len(absolutePathAStar)-1].XCoordinate != float32(20) && absolutePathAStar[len(absolutePathAStar)-1].YCoordinate != float32(20) {
+		t.Errorf("Result was incorrect, got X value: %f, Y value: %f ; Want X: %d, Y: %d.", absolutePathAStar[0].XCoordinate, absolutePathAStar[0].YCoordinate, 20*mazeSizeOriginal, 20*mazeSizeOriginal)
+
+	}
+}
+
+func TestSmallPathDijkstras(t *testing.T) {
+	var gameGridDFS [][]mazegrid.MazeSquare = algorithms.DFS(mazeSizeOriginal)
+
+	var dijkstrasPath = algorithms.Dijkstras(gameGridDFS, 20, 20, 20*mazeSizeOriginal, 20*mazeSizeOriginal)
+	var absolutePathDijkstras, weightDijkstras = algorithms.AbsolutePath(dijkstrasPath)
+
+	if len(absolutePathDijkstras) == 0 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", len(absolutePathDijkstras), "Path Length = 0")
+	} else if weightDijkstras == 0 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", weightDijkstras, "Number = 0 ")
+	} else if len(absolutePathDijkstras) == mazeSizeOriginal {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", len(absolutePathDijkstras), "Lenght of Maze ")
+	} else if weightDijkstras == mazeSizeOriginal {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", weightDijkstras, "Length of Maze ")
+	}
+}
+
+func TestMediumPathDijkstras(t *testing.T) {
+	mazeSizeOriginal = mazeSizeOriginal * 2
+	var gameGridDFS [][]mazegrid.MazeSquare = algorithms.DFS(mazeSizeOriginal)
+
+	var dijkstrasPath = algorithms.Dijkstras(gameGridDFS, 20, 20, 20*mazeSizeOriginal, 20*mazeSizeOriginal)
+	var absolutePathDijkstras, weightDijkstras = algorithms.AbsolutePath(dijkstrasPath)
+
+	if len(absolutePathDijkstras) == 0 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", len(absolutePathDijkstras), "Path Length = 0")
+	} else if weightDijkstras == 0 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", weightDijkstras, "Number = 0 ")
+	} else if len(absolutePathDijkstras) == mazeSizeOriginal {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", len(absolutePathDijkstras), "Lenght of Maze ")
+	} else if weightDijkstras == mazeSizeOriginal {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", weightDijkstras, "Length of Maze ")
+	}
+}
+
+func TestLargePathDijkstras(t *testing.T) {
+	mazeSizeOriginal = mazeSizeOriginal * mazeSizeOriginal
+
+	var gameGridDFS [][]mazegrid.MazeSquare = algorithms.DFS(mazeSizeOriginal)
+
+	var dijkstrasPath = algorithms.Dijkstras(gameGridDFS, 20, 20, 20*mazeSizeOriginal, 20*mazeSizeOriginal)
+	var absolutePathDijkstras, weightDijkstras = algorithms.AbsolutePath(dijkstrasPath)
+
+	if len(absolutePathDijkstras) == 0 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", len(absolutePathDijkstras), "Path Length = 0")
+	} else if weightDijkstras == 0 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", weightDijkstras, "Number = 0 ")
+	} else if len(absolutePathDijkstras) == mazeSizeOriginal {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", len(absolutePathDijkstras), "Lenght of Maze ")
+	} else if weightDijkstras == mazeSizeOriginal {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", weightDijkstras, "Length of Maze ")
+	}
+}
+
+func TestSmallPathAStar(t *testing.T) {
+	mazeSizeOriginal = 4
+	var gameGridDFS [][]mazegrid.MazeSquare = algorithms.DFS(mazeSizeOriginal)
+
+	var dijkstrasPath = algorithms.AStar(gameGridDFS, 20, 20, 20*mazeSizeOriginal, 20*mazeSizeOriginal)
+	var absolutePathDijkstras, weightDijkstras = algorithms.AbsolutePath(dijkstrasPath)
+
+	if len(absolutePathDijkstras) == 0 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", len(absolutePathDijkstras), "Path Length = 0")
+	} else if weightDijkstras == 0 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", weightDijkstras, "Number = 0 ")
+	} else if len(absolutePathDijkstras) == mazeSizeOriginal {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", len(absolutePathDijkstras), "Lenght of Maze ")
+	} else if weightDijkstras == mazeSizeOriginal {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", weightDijkstras, "Length of Maze ")
+	}
+}
+
+func TestMediumPathAStar(t *testing.T) {
+	mazeSizeOriginal = mazeSizeOriginal * 2
+	var gameGridDFS [][]mazegrid.MazeSquare = algorithms.DFS(mazeSizeOriginal)
+
+	var dijkstrasPath = algorithms.AStar(gameGridDFS, 20, 20, 20*mazeSizeOriginal, 20*mazeSizeOriginal)
+	var absolutePathDijkstras, weightDijkstras = algorithms.AbsolutePath(dijkstrasPath)
+
+	if len(absolutePathDijkstras) == 0 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", len(absolutePathDijkstras), "Path Length = 0")
+	} else if weightDijkstras == 0 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", weightDijkstras, "Number = 0 ")
+	} else if len(absolutePathDijkstras) == mazeSizeOriginal {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", len(absolutePathDijkstras), "Lenght of Maze ")
+	} else if weightDijkstras == mazeSizeOriginal {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", weightDijkstras, "Length of Maze ")
+	}
+}
+
+func TestLargePathAStar(t *testing.T) {
+	mazeSizeOriginal = mazeSizeOriginal * mazeSizeOriginal
+
+	var gameGridDFS [][]mazegrid.MazeSquare = algorithms.DFS(mazeSizeOriginal)
+
+	var dijkstrasPath = algorithms.AStar(gameGridDFS, 20, 20, 20*mazeSizeOriginal, 20*mazeSizeOriginal)
+	var absolutePathDijkstras, weightDijkstras = algorithms.AbsolutePath(dijkstrasPath)
+
+	if len(absolutePathDijkstras) == 0 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", len(absolutePathDijkstras), "Path Length = 0")
+	} else if weightDijkstras == 0 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", weightDijkstras, "Number = 0 ")
+	} else if len(absolutePathDijkstras) == mazeSizeOriginal {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", len(absolutePathDijkstras), "Lenght of Maze ")
+	} else if weightDijkstras == mazeSizeOriginal {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", weightDijkstras, "Length of Maze ")
+	}
+}
