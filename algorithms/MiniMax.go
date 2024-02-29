626b0eb9f8380da6fdc2aa51a457663e3b2cc3e5
Favinha Rui (2021) ZKAC432 <rui.favinha.2021@live.rhul.ac.uk>
2024-02-29 16:57:51 +0000

    Created Evaluation function for MiniMax

--- algorithms/MiniMax.go
+++ algorithms/MiniMax.go
@@ -1,3 +1,24 @@
 package algorithms
 
+import (
+	"gitlab.cim.rhul.ac.uk/zkac432/PROJECT/mazegrid"
+)
+
 func MiniMax() {}
+
+func evalPos(pacmanPos mazegrid.Position, pacmanPoints int, ghostPos mazegrid.Position, pellots []mazegrid.Position, isGhost bool) int {
+	totalEval := 0
+	nearestPellotPos, nearestPellotDistance := nearestPellot(pacmanPos, pellots)
+
+	if isGhost {
+		totalEval = totalEval + len(pellots)
+		distanceToNearestPacmanPellot := HeuristicsDistance(float64(ghostPos.XCoordinate), float64(ghostPos.YCoordinate), float64(nearestPellotPos.XCoordinate), float64(nearestPellotPos.YCoordinate))
+		totalEval = int((float64(totalEval) + distanceToNearestPacmanPellot) * -1)
+
+	} else {
+		totalEval = totalEval + pacmanPoints
+		totalEval = totalEval + int(nearestPellotDistance)
+	}
+
+	return totalEval
+}
