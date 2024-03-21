f7b49e77d213b3e6ab1e85f865c5167067edab10
Favinha Rui (2021) ZKAC432 <rui.favinha.2021@live.rhul.ac.uk>
2024-03-21 15:51:20 +0000

    Fixed a MiniMax bug as the square size wasn't correct

--- algorithms/PosToNodeInGrid.go
+++ algorithms/PosToNodeInGrid.go
@@ -3,12 +3,12 @@ package algorithms
 import "gitlab.cim.rhul.ac.uk/zkac432/PROJECT/mazegrid"
 
 // This function, given a array of positions and a game grid, turns the array of positions to an array of maze squares
-func PosToNode(gameGrid [][]mazegrid.MazeSquare, arrOfPos []mazegrid.Position) []mazegrid.MazeSquare {
+func PosToNode(gameGrid [][]mazegrid.MazeSquare, arrOfPos []mazegrid.Position, squareSize int) []mazegrid.MazeSquare {
 	var posToNodeArr []mazegrid.MazeSquare
 
 	for i := 0; i < len(arrOfPos); i++ {
-		firstArr := int((arrOfPos[i].YCoordinate / 20) - 1)
-		secondArr := int((arrOfPos[i].XCoordinate / 20) - 1)
+		firstArr := int((int(arrOfPos[i].YCoordinate) / squareSize) - 1)
+		secondArr := int((int(arrOfPos[i].XCoordinate) / squareSize) - 1)
 
 		posToNodeArr = append(posToNodeArr, gameGrid[firstArr][secondArr])
 
